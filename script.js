e43ddcde2abab8462444729faa2980988ffb4ab3
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-25 13:31:49 -0700

    finished up

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let hue = 0;
+let direction = true;
 
 function draw(e) {
   if (!isDrawing) return;
   console.log(e);
   ctx.strokeStyle = `hsl(${hue}, 100%, 50% )`;
+
   ctx.beginPath();
   //start from
   ctx.moveTo(lastX, lastY);
@@ -26,6 +28,17 @@ function draw(e) {
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY];
   hue++;
+  if (hue >= 360) {
+    hue = 0;
+  }
+  if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+    direction = !direction;
+  }
+  if (direction) {
+    ctx.lineWidth++;
+  } else {
+    ctx.lineWidth--;
+  }
 }
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
