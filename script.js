fd0e14313f4f27b201007547c676abea0e7ee3d6
Andrew McInally <realfunnyestate@yahoo.com>
2021-11-25 12:48:59 -0700

    working better

--- script.js
+++ script.js
@@ -9,3 +9,25 @@ ctx.lineJoin = "round";
 ctx.lineCap = "round";
 
 let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+
+function draw(e) {
+  if (!isDrawing) return;
+  console.log(e);
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(lastX, lastY);
+  //go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener("mousemove", draw);
+
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
